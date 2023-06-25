ecf2d0e4ed5b3ccca18424cdb3821299ea253835
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2023-06-26 00:44:13 +0200

    Add project files.

---
+++ STATE_PrepaidKartica/PrepaidForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace STATE_PrepaidKartica
+{
+    public partial class PrepaidForm : Form
+    {
+        private PrepaidKartica _kartica;
+        public PrepaidForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PrepaidForm_Load(object sender, EventArgs e)
+        {
+            _kartica = new PrepaidKartica("HR12312414213");
+            txtSerijskiBroj.Text = _kartica.SerijskiBroj;
+        }
+
+        private void btnAktiviraj_Click(object sender, EventArgs e)
+        {
+            _kartica.Aktiviraj();
+        }
+
+        private void btnUplati_Click(object sender, EventArgs e)
+        {
+            double iznosUplate = double.Parse(txtIznosUplate.Text);
+            _kartica.Uplati(iznosUplate);
+            txtIznosUplate.Clear();
+        }
+
+        private void btnIsplati_Click(object sender, EventArgs e)
+        {
+            double iznosIsplate = double.Parse(txtIznosIsplate.Text);
+            _kartica.Isplati(iznosIsplate);
+            txtIznosIsplate.Clear();
+        }
+    }
+}
