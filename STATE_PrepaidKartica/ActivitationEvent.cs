3c824b9ec92ba4b89bea2b85870845e7671b92b8
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2023-06-26 03:02:32 +0200

    Add enumerations ProjectState and ActivitationEvent

---
+++ STATE_PrepaidKartica/ActivitationEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STATE_PrepaidKartica
+{
+    public enum ActivitationEvent
+    {
+        Activation,
+        Payment,
+        WithdrawMoney,
+        NotEnoughMoney,
+        EnoughMoney,
+    }
+}
