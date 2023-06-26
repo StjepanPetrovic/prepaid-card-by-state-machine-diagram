325f2128f95b6660fb16ee378a6b760c3ea6a56b
Stjepan Petrović <stjepanpetrovic11@gmail.com>
2023-06-26 03:31:49 +0200

    Add class StateManager

---
+++ STATE_PrepaidKartica/StateManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STATE_PrepaidKartica
+{
+    internal class StateManager
+    {
+        public ProjectState CurrentState { get; set; }
+        private Dictionary<Transition, ProjectState> AllowedTransition { get; set; }
+
+        public StateManager()
+        {
+            CurrentState = ProjectState.NotActive;
+            SpecifyAllowedTransitions();
+        }
+
+        private void SpecifyAllowedTransitions()
+        {
+            AllowedTransition = new Dictionary<Transition, ProjectState>();
+
+            AllowedTransition.Add(new Transition(ProjectState.NotActive, ActivitationEvent.Activation), ProjectState.Active);
+            AllowedTransition.Add(new Transition(ProjectState.Active, ActivitationEvent.Payment), ProjectState.Active);
+            AllowedTransition.Add(new Transition(ProjectState.Active, ActivitationEvent.EnoughMoney), ProjectState.Active);
+            AllowedTransition.Add(new Transition(ProjectState.Active, ActivitationEvent.NotEnoughMoney), ProjectState.NotEnoughMoney);
+            AllowedTransition.Add(new Transition(ProjectState.NotEnoughMoney, ActivitationEvent.Payment), ProjectState.Active);
+        }
+
+        public void MakeTransition(ActivitationEvent activitationEvent)
+        {
+            Transition transition = new Transition(CurrentState, activitationEvent);
+            ProjectState resultingState;
+
+            if (AllowedTransition.TryGetValue(transition, out resultingState) == false)
+            {
+                throw new ApplicationException($"Transition {CurrentState} -> {activitationEvent} is not possible!");
+            }
+
+            CurrentState = resultingState;
+        }
+
+        public override string ToString()
+        {
+            return CurrentState.ToString();
+        }
+    }
+}
